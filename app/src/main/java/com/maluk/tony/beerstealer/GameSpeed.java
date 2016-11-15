238ef7e9e3889c81f5f455bb2d99fea85ed91bba
Anton Maliutenko <maluk.tony@gmail.com>
2016-11-15 03:44:57 +0200

    Refactored class

--- app/src/main/java/com/maluk/tony/beerstealer/GameSpeed.java
+++ app/src/main/java/com/maluk/tony/beerstealer/GameSpeed.java
@@ -65,5 +65,113 @@ public class GameSpeed {
             animLRD.setDuration(50);
             animLRD.setStartOffset(0);
         }
+        if(a >=40){
+            animationLU.setDuration(1000);
+            animationLU.setStartOffset(0);
+            animLL.setDuration(500);
+            animLL.setStartOffset(100);
+            animationLD.setDuration(800);
+            animationLD.setStartOffset(0);
+            animLLD.setDuration(100);
+            animLLD.setStartOffset(0);
+            animationRU.setDuration(1200);
+            animationRU.setStartOffset(0);
+            animLR.setDuration(500);
+            animLR.setStartOffset(100);
+            animationRD.setDuration(150);
+            animationRD.setStartOffset(0);
+            animLRD.setDuration(50);
+            animLRD.setStartOffset(0);
+        }
+        if(a >= 50){
+            animationLU.setDuration(250);
+            animationLU.setStartOffset(0);
+            animLL.setDuration(50);
+            animLL.setStartOffset(0);
+            animationLD.setDuration(800);
+            animationLD.setStartOffset(0);
+            animLLD.setDuration(500);
+            animLLD.setStartOffset(100);
+            animationRU.setDuration(250);
+            animationRU.setStartOffset(0);
+            animLR.setDuration(50);
+            animLR.setStartOffset(0);
+            animationRD.setDuration(900);
+            animationRD.setStartOffset(0);
+            animLRD.setDuration(500);
+            animLRD.setStartOffset(100);
+        }
+        if(a >= 60){
+            animationLU.setDuration(250);
+            animationLU.setStartOffset(0);
+            animLL.setDuration(50);
+            animLL.setStartOffset(0);
+            animationLD.setDuration(400);
+            animationLD.setStartOffset(0);
+            animLLD.setDuration(50);
+            animLLD.setStartOffset(0);
+            animationRU.setDuration(250);
+            animationRU.setStartOffset(0);
+            animLR.setDuration(50);
+            animLR.setStartOffset(0);
+            animationRD.setDuration(900);
+            animationRD.setStartOffset(0);
+            animLRD.setDuration(500);
+            animLRD.setStartOffset(100);
+        }
+        if(a >= 70){
+            animationLU.setDuration(500);
+            animationLU.setStartOffset(0);
+            animLL.setDuration(100);
+            animLL.setStartOffset(0);
+            animationLD.setDuration(800);
+            animationLD.setStartOffset(0);
+            animLLD.setDuration(100);
+            animLLD.setStartOffset(0);
+            animationRU.setDuration(500);
+            animationRU.setStartOffset(0);
+            animLR.setDuration(100);
+            animLR.setStartOffset(0);
+            animationRD.setDuration(300);
+            animationRD.setStartOffset(0);
+            animLRD.setDuration(100);
+            animLRD.setStartOffset(0);
+        }
+        if(a >= 80){
+            animationLU.setDuration(250);
+            animationLU.setStartOffset(0);
+            animLL.setDuration(50);
+            animLL.setStartOffset(0);
+            animationLD.setDuration(400);
+            animationLD.setStartOffset(0);
+            animLLD.setDuration(50);
+            animLLD.setStartOffset(0);
+            animationRU.setDuration(250);
+            animationRU.setStartOffset(0);
+            animLR.setDuration(50);
+            animLR.setStartOffset(0);
+            animationRD.setDuration(150);
+            animationRD.setStartOffset(0);
+            animLRD.setDuration(50);
+            animLRD.setStartOffset(0);
+        }
+        if(a >= 90){
+            animationLU.setDuration(150);
+            animationLU.setStartOffset(0);
+            animLL.setDuration(50);
+            animLL.setStartOffset(0);
+            animationLD.setDuration(200);
+            animationLD.setStartOffset(0);
+            animLLD.setDuration(50);
+            animLLD.setStartOffset(0);
+            animationRU.setDuration(150);
+            animationRU.setStartOffset(0);
+            animLR.setDuration(50);
+            animLR.setStartOffset(0);
+            animationRD.setDuration(150);
+            animationRD.setStartOffset(0);
+            animLRD.setDuration(50);
+            animLRD.setStartOffset(0);
+        }
     }
 }
