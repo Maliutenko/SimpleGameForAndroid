f3b91f26f1a3c25faa4f9082473b103f1690be54
Anton Maliutenko <maluk.tony@gmail.com>
2016-11-15 03:26:30 +0200

    Refactored class

--- app/src/main/java/com/maluk/tony/beerstealer/RulesActivity.java
+++ app/src/main/java/com/maluk/tony/beerstealer/RulesActivity.java
@@ -1,9 +1,12 @@
 package com.maluk.tony.beerstealer;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Tony on 29.08.2016.
@@ -11,11 +14,34 @@ import android.widget.Button;
 public class RulesActivity extends StartActivity implements View.OnClickListener{
 
     Button button;
+    TextView tvOne;
+    TextView tvTwo;
+    TextView tvThree;
+    TextView tvFour;
+    ImageView ivLightOne;
+    ImageView ivLightTwo;
+    ImageView ivDark;
+    ImageView ivRedArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        tvOne = (TextView)findViewById(R.id.text_rules_one);
+        tvTwo = (TextView)findViewById(R.id.text_rules_two);
+        tvThree = (TextView)findViewById(R.id.text_rules_three);
+        tvFour = (TextView)findViewById(R.id.text_rules_four);
+        ivLightOne = (ImageView)findViewById(R.id.image_view_light);
+        ivLightTwo = (ImageView)findViewById(R.id.image_view_light_two);
+        ivDark = (ImageView)findViewById(R.id.image_view_dark);
+        ivRedArrow = (ImageView)findViewById(R.id.image_view_arrow);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "matreshka.ttf");
+        tvOne.setTypeface(typeface);
+        tvTwo.setTypeface(typeface);
+        tvThree.setTypeface(typeface);
+        tvFour.setTypeface(typeface);
 
         button = (Button)findViewById(R.id.back_button);
         button.setOnClickListener(this);
